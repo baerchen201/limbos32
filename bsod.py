1f1c9da927fb4f2c4ce97d814ed5f4ac8a7bd2bf
baer1 <videocreator@outlook.de>
2024-10-28 01:47:33 +0100

    Remove some code

--- bsod.py
+++ bsod.py
@@ -1,36 +1,7 @@
 import ctypes
-
-
-# note here: I have no idea how this works (but somehow it DOES work).
-# I just found this piece of code online once and then ported it from PowerShell to python.
-# DO NOT TEST THIS ON YOUR PC IF YOU HAVE ANYTHING IMPORTANT OPENED, IT WILL CRASH YOUR PC INSTANTLY AND WITHOUT WARNING
-# -- baer1 (baerchen201)
-
-
 def bsod():
-    source = (
-        "\n"
-        "using System;\n"
-        "using System.Runtime.InteropServices;\n"
-        "public static class CS{\n"
-        '    [DllImport("ntdll.dll")]\n'
-        "    public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, "
-        "bool IsThreadPrivilege, out bool PreviousValue);\n"
-        '    [DllImport("ntdll.dll")]\n'
-        "    public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, "
-        "uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);\n"
-        "    public static unsafe void Kill(){\n"
-        "        Boolean tmp1;\n"
-        "        uint tmp2;\n"
-        "        RtlAdjustPrivilege(19, true, false, out tmp1);\n"
-        "        NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out tmp2);\n"
-        "    }\n"
-        "}\n"
-    )
-
     class CS(ctypes.Structure):
         pass
-
     CS_Kill = ctypes.windll.ntdll.NtRaiseHardError
     CS_Kill.argtypes = [
         ctypes.c_uint,
